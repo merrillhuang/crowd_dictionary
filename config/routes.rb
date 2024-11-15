4c5dda2d9fa68bedf5f19dc074308393d1e47a9d
Merrill Huang <127697576+merrillhuang@users.noreply.github.com>
2024-11-15 20:14:03 +0000

    install devise

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,4 @@ Rails.application.routes.draw do
   # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
 
   # get "/your_first_screen" => "pages#first"
-  
 end
