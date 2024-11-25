d61d1d33b228bdf8ce7b4ce2178bd19753853878
Merrill Huang <127697576+merrillhuang@users.noreply.github.com>
2024-11-25 02:45:44 +0000

    make submitter and origin for phrase optional to allow nil values for those columns

--- app/models/phrase.rb
+++ app/models/phrase.rb
@@ -8,16 +8,16 @@
 #  updated_at   :datetime         not null
 #  language_id  :integer          not null
 #  origin_id    :integer
-#  submitter_id :integer          default(0), not null
+#  submitter_id :integer
 #
 class Phrase < ApplicationRecord
-  belongs_to :submitter, required: true, class_name: "User", foreign_key: "submitter_id"
+  belongs_to :submitter, class_name: "User", foreign_key: "submitter_id", optional: true
 
   has_many  :ratings, class_name: "Rating", foreign_key: "phrase_id", dependent: :destroy
 
   has_many  :translations, class_name: "Phrase", foreign_key: "origin_id", dependent: :destroy
 
-  belongs_to :origin_word, required: true, class_name: "Phrase", foreign_key: "origin_id", counter_cache: :translations_count
+  belongs_to :origin_word, class_name: "Phrase", foreign_key: "origin_id", optional: true
 
   belongs_to :target_language, required: true, class_name: "Language", foreign_key: "language_id"
 end
