f232817e9edd7e3dbb0315713ea439a68966908a
Merrill Huang <127697576+merrillhuang@users.noreply.github.com>
2024-11-30 16:02:23 +0000

    add search route that redirects to show translations for phrase in target language or shows not_found page

--- app/controllers/phrases_controller.rb
+++ app/controllers/phrases_controller.rb
@@ -8,6 +8,8 @@ class PhrasesController < ApplicationController
 
   # GET /phrases/1 or /phrases/1.json
   def show
+    @translations = Phrase.where({"origin_id" => @phrase.id, "language_id" => params[:target_language]})
+    @target_language = Language.where("id" => params[:target_language])[0].name
   end
 
   # GET /phrases/new
@@ -57,6 +59,21 @@ class PhrasesController < ApplicationController
     end
   end
 
+  def search
+    @origin_content = params[:origin_word]
+    @origin_language = Language.where("id" => params[:starting_language])[0].name
+    @origin_word = Phrase.where({"content" => @origin_content, "origin_id" => nil, "language_id" => params[:starting_language]})
+    if ! @origin_word.empty?
+      respond_to do |format|
+        format.html { redirect_to phrase_url(@origin_word[0].id, target_language: params[:target_language]) }
+      end
+    else
+      respond_to do |format|
+        format.html { render :not_found }
+      end
+    end
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_phrase
@@ -65,6 +82,6 @@ class PhrasesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def phrase_params
-      params.require(:phrase).permit(:context, :submitter_id, :language_id, :origin_id)
+      params.require(:phrase).permit(:content, :submitter_id, :language_id, :origin_id)
     end
 end
