19d0b8530285137d64c4a2d1ff0d92e786dc5459
Merrill Huang <127697576+merrillhuang@users.noreply.github.com>
2024-12-08 21:52:40 +0000

    add validations for models

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -13,4 +13,8 @@ class Rating < ApplicationRecord
   belongs_to :submitter, required: true, class_name: "User", foreign_key: "submitter_id"
 
   belongs_to :phrase, required: true, class_name: "Phrase", foreign_key: "phrase_id"
+
+  validates :rating_val, presence: true
+  validates :phrase_id, presence: true
+  validates :submitter_id, presence: true
 end
