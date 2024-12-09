49008ef00d40d13c53d2fa5d0f9fda64abc8a585
Merrill Huang <127697576+merrillhuang@users.noreply.github.com>
2024-12-09 11:39:35 +0000

    include pundit in application_controller, show flash message when Pundit error, exclude devise_controller from pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   skip_forgery_protection
+
+  include Pundit::Authorization
+  after_action :verify_authorized, unless: :devise_controller?
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+    def user_not_authorized
+      flash[:alert] = "You are not authorized to perform this action."
+      
+      redirect_back fallback_location: root_url
+    end
 end
