6a27627e5a88bce50c8f949a0bf6d3a8197632a4
Merrill Huang <127697576+merrillhuang@users.noreply.github.com>
2024-12-10 06:31:07 +0000

    move calculation of average user rating to service

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,19 +3,7 @@ class UsersController < ApplicationController
 
   def show
     authorize User
-    sum = 0
-    num_ratings = 0
-    @avg_rating = 0
-    @user.translated_words.each do |phrase|
-      phrase.ratings.each do |rating|
-        sum += rating.rating_val.to_f
-        num_ratings += 1
-      end
-    end
-    
-    if num_ratings > 0
-      @avg_rating = (sum / num_ratings).truncate(2)
-    end
+    @avg_rating = AvgUserRating.call(@user)
 
     @translations = @user.translated_words.page(params[:page]).per(4)
   end
