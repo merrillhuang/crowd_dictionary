7e6f02fb6ffbcbb10c8669bffb23a7fb2ad843af
Merrill Huang <127697576+merrillhuang@users.noreply.github.com>
2024-12-10 10:16:41 +0000

    enable I18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   skip_forgery_protection
 
+  around_action :switch_locale
+
   include Pundit::Authorization
   after_action :verify_authorized, unless: :devise_controller?
 
@@ -12,5 +14,14 @@ class ApplicationController < ActionController::Base
     flash[:alert] = "You are not authorized to perform this action."
 
     redirect_back fallback_location: root_url
+  end
+
+  def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
   end
 end
