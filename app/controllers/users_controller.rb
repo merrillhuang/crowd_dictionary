afbd698e38e0adcbc16f10ce7e2a478a5e7a8e40
Merrill Huang <127697576+merrillhuang@users.noreply.github.com>
2024-12-10 06:27:43 +0000

    add pagination to user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,14 +3,21 @@ class UsersController < ApplicationController
 
   def show
     authorize User
-    @sum = 0
-    @num_ratings = 0
+    sum = 0
+    num_ratings = 0
+    @avg_rating = 0
     @user.translated_words.each do |phrase|
       phrase.ratings.each do |rating|
-        @sum += rating.rating_val.to_f
-        @num_ratings += 1
+        sum += rating.rating_val.to_f
+        num_ratings += 1
       end
     end
+    
+    if num_ratings > 0
+      @avg_rating = (sum / num_ratings).truncate(2)
+    end
+
+    @translations = @user.translated_words.page(params[:page]).per(4)
   end
 
   private
