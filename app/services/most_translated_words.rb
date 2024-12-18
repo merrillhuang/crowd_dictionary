f6ed7521aa3ad4bb67a7d17e5737c48c81fc2458
Merrill Huang <127697576+merrillhuang@users.noreply.github.com>
2024-12-18 21:06:20 +0000

    add comments to describe methods

--- app/services/most_translated_words.rb
+++ app/services/most_translated_words.rb
@@ -1,23 +1,28 @@
 class MostTranslatedWords
-  # Could use some comments
+  # Method to find the words with the most submitted translations for a particular language
   def self.call(language_id)
+    # Finding the words for a particular language
     origin_words_for_language = Phrase.where({ "language_id" => language_id, "origin_id" => nil })
     translations_count = {}
 
+    # Finding all the origin words for this language and saving the number of submitted translations they have
     origin_words_for_language.each do |word|
       translations_count[word.id] = word.translations.size
     end
 
+    # Sorts all origin words for this language by their number of submitted translations, grabbing top 5
     translations_count = translations_count.sort_by { |k, v| -v }.slice(0, 5)
 
     most_translated_words_ids = []
 
+    # Only grabs the words with more than 0 translations 
     translations_count.each do |subarray|
-      if subarray[1] != 0
+      if subarray[1] > 0
         most_translated_words_ids.append(subarray[0])
       end
     end
 
+    # Grabs and returns the ActiveRecord object representations of the most translated words
     most_translated_words = []
     most_translated_words_ids.each do |word_id|
       most_translated_words.append(Phrase.where({ "id" => word_id })[0])
